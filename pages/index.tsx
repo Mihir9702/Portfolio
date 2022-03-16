bf46809937b6cf462abcba9c75a1648153f8c584
Mihir Patel <mihirkpatel9702@gmail.com>
2022-03-16 08:01:57 -0400

    animation configured

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
+import Image from 'next/image'
+import sun from '../public/2k_sun.jpg'
 
 const Home: NextPage = () => {
   return (
@@ -20,6 +22,9 @@ const Home: NextPage = () => {
             <h1 className={styles.title}>Mihir</h1>
           </div>
         </header>
+        <div className={styles.section}>
+          <div className={styles.sun} />
+        </div>
       </div>
     </>
   )
