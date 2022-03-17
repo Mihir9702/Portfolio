009503853e833f4d8e84d1dabfbb7205b51679ad
Mihir Patel <mihirkpatel9702@gmail.com>
2022-03-17 17:39:03 -0400

    responsive nav bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,11 @@ const Header: React.FC = () => {
       <div className={styles.mstar} />
       <div className={styles.bstar} />
       <div className={styles.titleContainer}>
-        <h1 className={styles.title}>Mihir</h1>
+        <h1 className={styles.title}>M</h1>
+        <h1 className={styles.title}>i</h1>
+        <h1 className={styles.title}>h</h1>
+        <h1 className={styles.title}>i</h1>
+        <h1 className={styles.title}>r</h1>
       </div>
     </header>
   )
---
+++ src/components/MobileNavItems.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MobileNavItems = () => {
+  return (
+    <ul>
+      <li>Hello</li>
+      <li>Hello</li>
+      <li>Hello</li>
+      <li>Hello</li>
+      <li>Hello</li>
+    </ul>
+  )
+}
+
+export default MobileNavItems
