adda256e87d247de28109877f53a0c424bfbd621
Mihir Patel <mihirkpatel9702@gmail.com>
2022-03-23 16:17:52 -0400

    fixed links to code + project

--- src/Home/Card.tsx
+++ src/Home/Card.tsx
@@ -1,30 +1,54 @@
 import React from 'react'
 import Image from 'next/image'
 import Tags from './Tags'
+import Link from 'next/link'
 
 interface Props {
+  project: string
+  github: string
   src: StaticImageData
   title: string
   content?: string
   tags: string[]
 }
 
-const Card: React.FC<Props> = ({ src, title, content, tags }) => {
+const Card: React.FC<Props> = ({
+  project,
+  github,
+  src,
+  title,
+  content,
+  tags,
+}) => {
   return (
-    <div className="card mt-8 flex max-w-sm flex-col items-center justify-evenly overflow-hidden rounded-xl">
+    <div className="card mt-8 flex max-w-sm flex-col justify-center overflow-hidden rounded-xl">
       <div className="absolute top-0 w-full">
         <Image src={src} alt="website" />
       </div>
 
       <div className="mt-48 px-6 py-6">
         <div className="mb-2 text-xl font-bold">{title}</div>
-        <p className="text-base text-gray-700">{content}</p>
-        <button className="mt-6 w-full rounded-xl bg-gray-800 text-white shadow-md transition-all hover:bg-gray-600">
-          Website
-        </button>
-        <button className="mt-6 w-full rounded-xl bg-gray-800 text-white shadow-md transition-all hover:bg-gray-600">
-          Source Code
-        </button>
+        <p
+          className={`${
+            title === 'MyCookbook' ? 'pb-4' : 'pb-2'
+          } text-base text-gray-700`}
+        >
+          {content}
+        </p>
+        <Link href={project}>
+          <a>
+            <button className="mt-6 w-full rounded-xl bg-gray-800 text-white shadow-md transition-all hover:bg-gray-600">
+              Website
+            </button>
+          </a>
+        </Link>
+        <Link href={github}>
+          <a>
+            <button className="mt-6 w-full rounded-xl bg-gray-800 text-white shadow-md transition-all hover:bg-gray-600">
+              Source Code
+            </button>
+          </a>
+        </Link>
       </div>
 
       <div className="px-6 pt-4 pb-2">
--- src/Home/Projects.tsx
+++ src/Home/Projects.tsx
@@ -16,12 +16,16 @@ const Projects: React.FC = () => {
         </div>
         <div className="items-center gap-x-20 md:flex">
           <Card
+            project={'/'}
+            github={'https://github.com/Mihir9702/Portfolio'}
             src={site}
             title="Portfolio"
             content="Styling this site was super fun and easy using Tailwind! Want to see how I made this site? Check out the code on GitHub!"
             tags={['TypeScript', 'NextJS', 'TailwindCSS']}
           />
           <Card
+            project={'https://makemybook.netlify.app/'}
+            github={'https://github.com/Mihir9702/MyCookbookClient'}
             src={mycb}
             title="MyCookbook"
             content="Always hungry but never know what to eat? Check out MyCookbook for a variety of recipes and you can make your own personal cookbooks!"
@@ -37,6 +41,8 @@ const Projects: React.FC = () => {
             ]}
           />
           <Card
+            project={'https://mono-trivia.herokuapp.com/'}
+            github={'https://github.com/Mihir9702/Trivia'}
             src={mt}
             title="MonoTrivia"
             content="Are you starring on a gameshow tomorrow night or do you just want to see how smart you really are? Check out MonoTrivia! It has questions ranging from easy-hard to improve your skills!"
@@ -51,6 +57,8 @@ const Projects: React.FC = () => {
             ]}
           />
           <Card
+            project={'https://mihir9702.github.io/JUMP/'}
+            github={'https://github.com/Mihir9702/JUMP'}
             src={jump}
             title="Jump"
             content={`"I've played Boshy, I've played Super Mario, I've played everything! Is there anything good out there?" Look no further because we got Jump, a parallax side scrolling platform game!`}
