51d9c90add97a4d48401b4ea1eee110bed511931
Mihir9702 <mkpatel9702@gmail.com>
2024-08-16 18:14:59 -0400

    update

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -4,16 +4,13 @@ const AboutSection = () => {
   return (
     <div className="flex flex-col items-start text-center">
       <h1 className="mt-12 text-4xl font-black leading-tight tracking-tight text-gray-900">
-        Who am I?
+        Who is he?
       </h1>
       <h2 className="mt-6 whitespace-nowrap max-w-3xl text-md md:mx-0 mx-auto md:text-xl leading-8 text-gray-600">
-        {'>'} Developing apps since 2020 (5 years now)
+        {'>'} Developing apps for over 6+ years
       </h2>
       <h2 className="mt-6 whitespace-nowrap max-w-3xl text-md md:mx-0 mx-auto md:text-xl leading-8 text-gray-500">
         {'>'} Full Stack Web & Mobile Development
-      </h2>
-      <h2 className="mt-6 whitespace-nowrap max-w-3xl text-md md:mx-0 mx-auto md:text-xl leading-8 text-gray-500">
-        {'>'} I am always testing my limits
       </h2>
     </div>
   )
